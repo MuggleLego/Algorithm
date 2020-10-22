03e11b4f08417507a5c9ac1fb2a929e7d8672b1e
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2020-10-22 22:34:40 +0800

    Create queue.go

---
+++ Data Structure/GO/queue.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+//type node struct {
+//	data interface{}
+//	next *node
+//}
+
+type queue struct {
+	head *node //just a dummy
+	tail *node
+}
+
+func iniNode(t interface{}) *node {
+	return &node{t, nil}
+}
+
+func newQueue() *queue {
+	h := iniNode(0)
+	return &queue{h, nil}
+}
+
+func (q *queue) enqueue(t interface{}) {
+	n := iniNode(t)
+	if q.tail == nil { //an empty queue
+		q.head.next = n
+		q.tail = n
+		return
+	}
+	q.tail.next = n
+	q.tail = n
+}
+
+func (q *queue) dequeue() (interface{}, error) {
+	if q.head.next == nil {
+		return nil, errors.New("empty queue")
+	}
+	tmp := q.head.next
+	q.head.next = tmp.next
+	return tmp.data, nil
+}
+
+func (q *queue) printQueue() {
+	fmt.Println("Head")
+	tmp := q.head.next
+	for tmp != nil {
+		fmt.Println(tmp.data)
+		tmp = tmp.next
+	}
+	fmt.Println("Tail")
+	fmt.Println()
+}
